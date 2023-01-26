567cb765b7ebd5622b60d3fd3b183357fce7d65d
Jason Hall <jason@chainguard.dev>
2023-01-26 11:28:34 -0500

    don't depend on sponge

--- crawl.sh
+++ crawl.sh
@@ -11,9 +11,9 @@ while read l; do
   mkdir -p $(dirname $fn)
   curl -s $l | jq > $fn
   if [ "$(jq 'has("keys")' $fn)" == "true" ]; then
-    jq -S -s 'sort_by(.keys[].kid)' $fn | sponge $fn
+    jq -S -s 'sort_by(.keys[].kid)' $fn > ${TMPDIR}/out && mv ${TMPDIR}/out $fn
   fi
   if grep -q "BEGIN CERTIFICATE" $fn; then
-    jq -S '.' $fn | sponge $fn
+    jq -S '.' $fn > ${TMPDIR}/out && mv ${TMPDIR}/out $fn
   fi
 done < urls.txt
