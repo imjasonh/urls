216884e292c04f868abb5f53ce4cd23b21d98037
Jason Hall <jason@chainguard.dev>
2023-01-26 11:56:33 -0500

    reproducibility fix didn't fix

--- crawl.sh
+++ crawl.sh
@@ -10,10 +10,4 @@ while read l; do
   echo $fn
   mkdir -p $(dirname $fn)
   curl -s $l | jq > $fn
-  if [ "$(jq 'has("keys")' $fn)" == "true" ]; then
-    jq -S -s 'sort_by(.keys[].kid)' $fn > ${TMPDIR}/out && mv ${TMPDIR}/out $fn
-  fi
-  if grep -q "BEGIN CERTIFICATE" $fn; then
-    jq -S '.' $fn > ${TMPDIR}/out && mv ${TMPDIR}/out $fn
-  fi
 done < urls.txt
