c1235016b21d882b6ca4e7700164f2da3f515cc2
Jason Hall <jason@chainguard.dev>
2023-01-27 11:22:33 -0500

    tmp better

--- crawl.sh
+++ crawl.sh
@@ -12,9 +12,11 @@ while read l; do
   curl -s $l | jq > $fn
 done < urls.txt
 
+tmp=$(mktemp)
+
 # sort keys for better diffs
-jq -S '.' www.googleapis.com/oauth2/v1/certs > ${TMPDIR}/tmp && mv ${TMPDIR}/tmp www.googleapis.com/oauth2/v1/certs
+jq -S '.' www.googleapis.com/oauth2/v1/certs > ${tmp} && mv ${tmp} www.googleapis.com/oauth2/v1/certs
 
 # sort keys and items for better diffs
-jq -S '.keys = (.keys | sort_by("kid"))' www.googleapis.com/oauth2/v2/certs > ${TMPDIR}/tmp && mv ${TMPDIR}/tmp www.googleapis.com/oauth2/v2/certs 
-jq -S '.keys = (.keys | sort_by("kid"))' www.googleapis.com/oauth2/v3/certs > ${TMPDIR}/tmp && mv ${TMPDIR}/tmp www.googleapis.com/oauth2/v3/certs 
+jq -S '.keys = (.keys | sort_by("kid"))' www.googleapis.com/oauth2/v2/certs > ${tmp} && mv ${tmp} www.googleapis.com/oauth2/v2/certs 
+jq -S '.keys = (.keys | sort_by("kid"))' www.googleapis.com/oauth2/v3/certs > ${tmp} && mv ${tmp} www.googleapis.com/oauth2/v3/certs 
