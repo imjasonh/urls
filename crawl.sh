749ac1516272b85769f77d927fed7045f6fdc3e4
Jason Hall <jason@chainguard.dev>
2023-01-27 11:21:09 -0500

    sort using jq

--- crawl.sh
+++ crawl.sh
@@ -11,3 +11,10 @@ while read l; do
   mkdir -p $(dirname $fn)
   curl -s $l | jq > $fn
 done < urls.txt
+
+# sort keys for better diffs
+jq -S '.' www.googleapis.com/oauth2/v1/certs > ${TMPDIR}/tmp && mv ${TMPDIR}/tmp www.googleapis.com/oauth2/v1/certs
+
+# sort keys and items for better diffs
+jq -S '.keys = (.keys | sort_by("kid"))' www.googleapis.com/oauth2/v2/certs > ${TMPDIR}/tmp && mv ${TMPDIR}/tmp www.googleapis.com/oauth2/v2/certs 
+jq -S '.keys = (.keys | sort_by("kid"))' www.googleapis.com/oauth2/v3/certs > ${TMPDIR}/tmp && mv ${TMPDIR}/tmp www.googleapis.com/oauth2/v3/certs 
