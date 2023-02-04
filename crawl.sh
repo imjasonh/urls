a0a34f076a46d2be8d1c3e4cd4aa9393ce778b7a
Jason Hall <jason@chainguard.dev>
2023-02-03 22:32:07 -0500

    try to get better diffs again still

--- crawl.sh
+++ crawl.sh
@@ -14,9 +14,9 @@ done < urls.txt
 
 tmp=$(mktemp)
 
-# sort keys for better diffs
+# Sort keys for better diffs.
 jq -S '.' www.googleapis.com/oauth2/v1/certs > ${tmp} && mv ${tmp} www.googleapis.com/oauth2/v1/certs
+jq -S '.keys = (.keys | sort_by("kid"))' appleid.apple.com/auth/keys > ${tmp} && mv ${tmp} appleid.apple.com/auth/keys
 
-# sort keys and items for better diffs
-jq -S '.keys = (.keys | sort_by("kid"))' www.googleapis.com/oauth2/v2/certs > ${tmp} && mv ${tmp} www.googleapis.com/oauth2/v2/certs 
-jq -S '.keys = (.keys | sort_by("kid"))' www.googleapis.com/oauth2/v3/certs > ${tmp} && mv ${tmp} www.googleapis.com/oauth2/v3/certs 
+# Ignore changes where rate limit is exceeded.
+if grep "API rate limit exceeded" api.github.com/meta; then git checkout -- api.github.com/meta; fi
