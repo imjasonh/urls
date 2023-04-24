08db2e677111776faea6b9b7783da02be006cc50
Jason Hall <jason@chainguard.dev>
2023-04-24 08:48:39 -0400

    sort more better
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- crawl.sh
+++ crawl.sh
@@ -16,7 +16,9 @@ tmp=$(mktemp)
 
 # Sort keys for better diffs.
 jq -S '.' www.googleapis.com/oauth2/v1/certs > ${tmp} && mv ${tmp} www.googleapis.com/oauth2/v1/certs
-jq -S '.keys = (.keys | sort_by("kid"))' appleid.apple.com/auth/keys > ${tmp} && mv ${tmp} appleid.apple.com/auth/keys
+jq -S '.keys |= sort_by(.kid)' appleid.apple.com/auth/keys > ${tmp} && mv ${tmp} appleid.apple.com/auth/keys
+jq -S '.keys |= sort_by(.kid)' www.googleapis.com/oauth2/v2/certs > ${tmp} && mv ${tmp} www.googleapis.com/oauth2/v2/certs
+jq -S '.keys |= sort_by(.kid)' www.googleapis.com/oauth2/v3/certs > ${tmp} && mv ${tmp} www.googleapis.com/oauth2/v3/certs
 
 # Ignore changes where rate limit is exceeded.
 if grep "API rate limit exceeded" api.github.com/meta; then git checkout -- api.github.com/meta; fi
